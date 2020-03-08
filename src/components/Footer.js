84d8c4b357df0a027e923826096ee9bd947d1b56
Brandon H Bodine <brandonhbodine@gmail.com>
2020-03-07 20:37:23 -0700

    Attempting to delete instances of products

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -34,11 +34,7 @@ const Footer = class extends React.Component {
                         About
                       </Link>
                     </li>
-                    <li>
-                      <Link className="navbar-item" to="/products">
-                        Products
-                      </Link>
-                    </li>
+
                     <li>
                       <Link className="navbar-item" to="/contact/examples">
                         Form Examples
