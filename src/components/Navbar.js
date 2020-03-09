571e4119c2de2d352f281cf451db9301ef9d2e9a
Brandon H Bodine <brandonhbodine@gmail.com>
2020-03-08 21:17:42 -0600

    More style updates. Need to learn Bulma

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,8 @@
 import React from 'react'
 import { Link } from 'gatsby'
 
+import logo from '../img/BB.svg'
+
 const Navbar = class extends React.Component {
   constructor(props) {
     super(props)
@@ -40,8 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-                BRANDON BODINE
-              {/* <img src={logo} alt="Kaldi" style={{ width: '88px' }} /> */}
+              <img src={logo} alt="BB" style={{ width: '90px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
